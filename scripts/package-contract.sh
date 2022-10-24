69b7683662529555e467e52bb0226bda74e1d6fd
Marc Fornos <frozen.pomelo@gmail.com>
2022-10-24 10:52:42 +0200

    fix build path for debug info

--- scripts/package-contract.sh
+++ scripts/package-contract.sh
@@ -6,17 +6,23 @@
 set -eu
 
 SRC_ROOT="${SRC_ROOT:-/build}"
-TMP_PACKAGE_BASE=/tmp/package
+TMP_PACKAGE_BASE="$SRC_ROOT/package"
 TMP_PACKAGE_SRC="$TMP_PACKAGE_BASE/src"
 TMP_PACKAGE_TARGET="$TMP_PACKAGE_SRC/target"
 PACKAGE_DST="${SRC_ROOT}/target/ink/package.zip"
+
+# We pin the root source path to emit stable dir names
+# on debug info (to `/build/package/src`).
+# When/if cargo-contracts supports adding RUSTC_FLAGS we could
+# use 'remap-path-prefix'
+mkdir -p "$TMP_PACKAGE_SRC"
+cp -r ${SRC_ROOT}/* "$TMP_PACKAGE_SRC"
+SRC_ROOT="$TMP_PACKAGE_SRC"
 
 # Build contract
 . /usr/local/bin/build-contract
 
 # Build verification package
-mkdir -p "$TMP_PACKAGE_SRC"
-cp -r ${SRC_ROOT}/* "$TMP_PACKAGE_SRC"
 mv ${TMP_PACKAGE_TARGET}/ink/*.contract "${TMP_PACKAGE_BASE}"
 rm -rf "$TMP_PACKAGE_TARGET"
 (cd "${TMP_PACKAGE_BASE}" && zip -r - src/ *.contract) > "$PACKAGE_DST"
